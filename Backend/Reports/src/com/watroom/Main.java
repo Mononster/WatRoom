6108a2d64e0a10a5de19af75faafa04872a9c8a1
TheLegendaryDuke <454048573@qq.com>
2017-07-13 19:41:03 -0400

    added time stamp for reports

--- Backend/Reports/src/com/watroom/Main.java
+++ Backend/Reports/src/com/watroom/Main.java
@@ -8,6 +8,11 @@ public class Main {
         String buildings = http.get(firebaseURL+"buildings.json");
 
         JSONObject reports = new JSONObject();
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        reports.put("last update: ", dateFormat.format(date)); //2016/11/16 12:08:43
+
         JSONObject buildingsJSON = new JSONObject(buildings);
         for(String buildingCode : buildingsJSON.keySet()) {
             JSONObject buildingJSON = buildingsJSON.getJSONObject(buildingCode);
